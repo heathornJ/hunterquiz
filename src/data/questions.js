a40f2585abf4c297f9b44a67e48cee7dda03a948
JetRhino <heathornj@gmail.com>
2024-09-09 10:45:03 +0100

    Add placeholder questions to questions array

--- src/data/questions.js
+++ src/data/questions.js
@@ -19,7 +19,141 @@ const questions = [
             {text:"false", isCorrect: false},
         ],
         type: "true-false"
-    }
+    },
+    {
+        id: 2,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+    {
+        id: 3,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+    {
+        id: 4,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+    {
+        id: 5,
+        question: "This is the first true or false question",
+        answers: [
+            {text:"true", isCorrect: true},
+            {text:"false", isCorrect: false},
+        ],
+        type: "true-false"
+    },
+    {
+        id: 6,
+        question: "This is the first true or false question",
+        answers: [
+            {text:"true", isCorrect: true},
+            {text:"false", isCorrect: false},
+        ],
+        type: "true-false"
+    },
+    {
+        id: 7,
+        question: "This is the first true or false question",
+        answers: [
+            {text:"true", isCorrect: true},
+            {text:"false", isCorrect: false},
+        ],
+        type: "true-false"
+    },
+    {
+        id: 8,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+    {
+        id: 9,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+    {
+        id: 10,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+    {
+        id: 11,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+    {
+        id: 12,
+        question: "This is the first true or false question",
+        answers: [
+            {text:"true", isCorrect: true},
+            {text:"false", isCorrect: false},
+        ],
+        type: "true-false"
+    },
+    {
+        id: 13,
+        question: "This is the first true or false question",
+        answers: [
+            {text:"true", isCorrect: true},
+            {text:"false", isCorrect: false},
+        ],
+        type: "true-false"
+    },
+    {
+        id: 14,
+        question: "This is the first question",
+        answers: [
+            {text:"correctAnswer", isCorrect: true},
+            {text:"b", isCorrect: false},
+            {text:"c", isCorrect: false},
+            {text:"d", isCorrect: false}
+        ],
+        type: "multiple-choice"
+    },
+
 ];
 
 export default questions;
